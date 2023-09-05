526a28294c0624c047a6f6ae350122d2ecebecb5
185953 <90598132+185953@users.noreply.github.com>
2023-09-05 11:43:44 +0530

    Update execute_sql_to_gcs_dag.py

--- dags/execute_sql_to_gcs_dag.py
+++ dags/execute_sql_to_gcs_dag.py
@@ -1,18 +1,23 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
-from airflow.operators.email import EmailOperator
+from airflow.utils.dates import days_ago
 import subprocess
 
- 
-
 def execute_sql_script():
-    # Your SQL execution logic here
-    pass
+    gcs_blob_path = "gs://taskbucket1/my_folder/empscript.sql"
+    gsutil_cmd = ["gsutil", "cat", gcs_blob_path]
 
- 
+    try:
+        sql_script = subprocess.check_output(gsutil_cmd, universal_newlines=True, stderr=subprocess.STDOUT)
+        client = bigquery.Client()
+        query_job = client.query(sql_script)
+        for row in query_job:
+            print(row)
+    except subprocess.CalledProcessError as e:
+        print("Error executing gsutil command:", e.output)
 
-default_args = {
+ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2023, 8, 8),
@@ -20,12 +25,10 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
- 
-
 with DAG(
     "execute_sql_to_gcs_dag",
     default_args=default_args,
-    schedule=None,
+    schedule_interval=None, 
     catchup=False,
 ) as dag:
 
@@ -35,3 +38,7 @@ with DAG(
         task_id="execute_sql_task",
         python_callable=execute_sql_script,
     )
+
+ 
+
+    execute_sql_task
